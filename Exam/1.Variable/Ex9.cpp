289cb02c797d6184637766ef12ccd3e6db7073a4
thu-hoai1004 <hoaibeobeo2001@gmail.com>
2023-10-14 00:04:12 +0700

    Update Exam Variable

---
+++ Exam/1.Variable/Ex9.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <math.h>
+const float PI = 3.14 ;
+using namespace std ;
+
+int main ()
+{
+    float r ;
+    cin >> r ;
+    cout << " Chu vi hinh tron = " << 2 << " * " << r << " * " << PI << " = " << 2 * r * PI << endl;
+    cout << " Dien tich hinh tron = " << r << " * " << r << " * " << PI << " = " << r * r * PI ;
+    return 0 ;
+}
