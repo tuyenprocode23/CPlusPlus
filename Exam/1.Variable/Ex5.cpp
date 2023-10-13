ddec24459f4dbf51e9a5300762275b24713d04a8
thu-hoai1004 <hoaibeobeo2001@gmail.com>
2023-10-13 23:21:47 +0700

    Update exam variable

---
+++ Exam/1.Variable/Ex5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std ;
+
+int main ()
+{
+    int i , j , k ;
+    i = j ;
+    j = k ;
+    cin >> k ;
+    cout << " i = " << i ;
+    return 0 ;
+}
---
+++ Exam/1.Variable/Ex7.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std ;
+
+int main ()
+{
+    float can_nang, chieu_cao ;
+    cin >> can_nang >> chieu_cao ;
+    cout << " BIM " << can_nang << " / " << chieu_cao << " * " << chieu_cao << " = " << can_nang / ( chieu_cao * chieu_cao ) << endl ;
+    return 0 ;
+}
