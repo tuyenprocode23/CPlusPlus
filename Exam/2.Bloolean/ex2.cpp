f478fb2ebe176d33c96b3bb2e3dd0e07abcc587e
thu-hoai1004 <hoaibeobeo2001@gmail.com>
2023-10-16 23:26:47 +0700

    update ex2

---
+++ Exam/2.Bloolean/ex2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std ;
+
+int main()
+{
+    float a, b ;
+    cin >> a >> b ;
+    cout <<  a << " + " << b << " = " << a + b << endl ;
+    cout << a << " - " << b << " = " << a - b << endl;
+    cout << a << " / " << b << " = " << a / b;
+    return 0 ;
+}
